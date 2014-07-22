33abcfbd61529ef4ec825df0114f7f212b140dc6
ccwalkerjm <ccwalkerjm@yahoo.com>
2014-07-22 13:50:37 -0500

    add  registration
    change names

--- html/js/cform.js
+++ html/js/cform.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $("#ajax-contact-form").submit(function () {
         var str = $(this).serialize();
+        $("#spinner").show();
         $.ajax({
             type: "POST",
             url: "http://primcms.courserv.com/contact/",
@@ -8,6 +9,7 @@ $(document).ready(function () {
             dataType: 'json',
             async: false,
             success: function (r) {
+                $("#spinner").hide();
                 if (r.isError == 0) // Message Sent? Show the 'Thank You' message and hide the form
                 {
                     result = '<div class="notification_ok">Your message has been sent. Thank you!<br> <a href="#" onclick="freset();return false;">send another mail</a></div>';
